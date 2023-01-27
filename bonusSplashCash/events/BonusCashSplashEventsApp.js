8547c9ca5ab3d6bc7b5ab7456efe19f0a9fe2260
artempopereka <artem.popereka@gmail.com>
2023-01-27 16:13:58 +0300

    js removed

--- bonusSplashCash/events/BonusCashSplashEventsApp.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BonusCashSplashEventsApp = void 0;
-exports.BonusCashSplashEventsApp = {
-    CASH_SPLASH_ANIMATION_COMPLETE: "BonusCashSplashEventsApp.CASH_SPLASH_ANIMATION_COMPLETE"
-};
--- bonusSplashCash/events/BonusCashSplashIntentsApp.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BonusCashSplashIntentsApp = void 0;
-exports.BonusCashSplashIntentsApp = {
-    SHOW_CASH_SPLASH: "BonusCashSplashIntentsApp.SHOW_CASH_SPLASH"
-};
